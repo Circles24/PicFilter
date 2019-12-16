91d5554fc9df13beb3c29944f9b57c4cfbc710ae
Suraj Singh Rana <surajsinghrana7417@gmail.com>
2019-12-16 23:51:14 +0530

    complete skeleton

--- Server.java
+++ Server.java
@@ -11,6 +11,8 @@ public class Server implements Runnable
     public Server(int portNo) throws Exception
     {
 
+        System.out.println("@Server.Server");
+
         resManager = ResourceManager.getInstance();
 
         sskt = new ServerSocket(portNo);
@@ -19,19 +21,66 @@ public class Server implements Runnable
     }
 
     public void run()
-    {
+    {   
 
+        System.out.println("@Server.run");
+
+        try{
+
+            Socket skt;
+            ClientHandler clHandler;
+
+            while(true){
+
+                skt = sskt.accept();
+
+                System.out.println("New Connection Req :: " + (InetSocketAddress)skte.getRemoteSocketAddress());
+
+                if(resManager.ClientHandlerAvailable()){
+
+                    clHandler = resManager.getClientHandler();
+
+                    clHandler.init(skt);
+
+                    clhandler.interrupt();
+
+                }
+
+            }
+
+        }
+
+        catch(Exception ex){
+
+            System.out.println("Exception@Server.run :: "+ex.getMessage());
+
+            ex.printStackTrace();
+
+        }
 
     }
 
     public static void main(String args[])
     {
+        System.out.println("@Server.main");
+
         try{
 
+            if(args.length != 1)throw new Exception("Usage :: java Server <portNo>");
+
+            int portNo = Integer.parseInt(args[0]);
+
+            new Server(portNo);
 
         }
 
-        catch(ArrayIndex)
+        catch(Exception ex){
+
+            System.out.println("Exception@Server.main :: "+ex.getMessage());
+
+            ex.printStackTrace();
+
+        }
     }
 
 }
